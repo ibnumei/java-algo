92deef42be072338190e22e5c68cee58c53fe435
ibnumei <ibnu.fauziyai@gmail.com>
2024-06-08 22:44:34 +0700

    initial commit

---
+++ src/main/java/com/algo/algo/leetCode/IsSubsequence_329.java
@@ -0,0 +1,16 @@
+package com.algo.algo.leetCode;
+
+public class IsSubsequence_329 {
+    public boolean isSubsequence(String s, String t) {
+        int sp = 0;
+        int tp = 0;
+
+        while(sp < s.length() && tp < t.length()) {
+            if(s.charAt(sp) == t.charAt(tp)) {
+                sp++;
+            }
+            tp++;
+        }
+        return sp == s.length();
+    }
+}
